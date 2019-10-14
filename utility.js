dd440e570290b574c9b32a770bb524c4d5312b85
chotung <choelestung@gmail.com>
2019-10-13 23:48:08 -0400

    cleaned up the code

--- utility.js
+++ utility.js
@@ -1,31 +1,47 @@
-/**
- * gets geolocation of the browser
- */
+var map;
+var service;
+var infowindow;
 
+// 40.7076864
+// -73.9950592
 
+function initialize() {
+   
+  // var locale = new google.maps.LatLng(-33.867, 151.195);
+  let startingPosition
 
-
-// convert miles to km for google
-// 1 * 1609.34 = km
-// location lat/long
-
-
-// INITIAL LOCATION
-const initialLocation = () => {
-  // Checks for geolocation api 
+    // Checks for geolocation api 
   if ("geolocation" in navigator) {
+    var locale
     /* geolocation is available */
     navigator.geolocation.getCurrentPosition(function (position) {
       const {
         latitude,
         longitude
       } = position.coords
-      const startingPosition = {
+      
+      startingPosition = {
         latitude,
         longitude
       }
       console.log(startingPosition)
-      return startingPosition
+      locale = new google.maps.LatLng(startingPosition.latitude, startingPosition.longitude)
+      console.log(locale)
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: locale,
+        zoom: 15
+      });
+
+      var request = {
+        location: locale,
+        // can't use floats 1609.34 is the conversion for meters
+        radius: '1609',
+        //just need to add some dynamic choices
+        type: ['restaurant']
+      };
+
+      service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, callback);
     })
   } else {
     /* geolocation IS NOT available */
@@ -33,71 +49,60 @@ const initialLocation = () => {
       message: "Unable to retrieve your location"
     })
   }
+
+  
+
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+
+      // 
+      // console.log(place)
+      console.log(place.name)
+      // console.log(place.geometry.location)
+      console.log(place.geometry.location.lat())
+      console.log(place.geometry.location.lng())
+      // createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 
-// Takes starting position 
-// scans in an area
-/**
- * {
- *  resturants = food/water,
- *  conviences stores = water/food
- *  hospitals/pharm/practices = meds
- * }
- */
-// GETS NEW LOCATION
+const newLocation = ((startPosition, playerDecision) => {
 
+  switch (playerDecision) {
+    case 'food':
+      sanitizeData(playerDecision)
+      break;
+    case 'water':
+      sanitizeData(playerDecision)
+      break;
+    case 'meds':
+      sanitizeData(playerDecision)
+      break;
+    default:
 
+  }
 
+  // console.log('testing function')
+})()
 
-// AJAX CALL FOR AVAILABLE PLACES
-const sanitizeData = (choice) => {
-    const choiceOfLocations = []
-    let newPosition = ''
-    // takes choice
-    // ajax call places api to populate the array
-    // if (choice > 10) {slice it down to 10}
-    // push all choice into locationsArray
- 
-    // create a random index to return 
-    // randomizer(choiceOfLocations.length)
-
-    // return choiceOfLocation[randomIndex] which is a new position
-    // Returns an location object with lat and long
+const randomizer = (numOfLocations) => {
+  return Math.floor(Math.random() * Math.floor(numOfLocations));
 }
-
-// sanitizeData()
-
-// RANDOM NUMBER GENERATOR
-
-// randomize a number 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
